4d8431bad6480e468733790ca0152a1a414050fa
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2023-11-08 13:44:05 -0500

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -233,7 +233,7 @@
             <div class="row"> <!-- opens row for second category -->
 
                 <div class="TOC-column">
-                    <h3>Fiction</h3>
+                    <h3>Short Stories</h3>
                 </div>
             </div>
                 <div class="row">
