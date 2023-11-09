2b7cfe8cb699f6cfd8cfb17a2fc3fecf013ac279
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2023-11-09 09:36:12 -0500

    Update functions.php
    modified kses

--- functions.php
+++ functions.php
@@ -246,6 +246,21 @@ add_filter('the_content', 'shenAleph_stanza_break');
 add_filter('get_the_author_meta_description', 'wpautop');
 
 
+/**
+ * Modify the list of allowed HTML tags in user descriptions.
+ * This function adds the 'strong' tag to the list of allowed tags
+ * when the 'user_description' context is being filtered.
+ */
+
+function my_kses_allowed_html($tags, $context) {
+    if ($context == 'user_description') {
+        $tags['strong'] = array();
+    }
+    return $tags;
+}
+
+// Apply the filter to modify allowed HTML tags
+add_filter('wp_kses_allowed_html', 'my_kses_allowed_html', 10, 2);
 
 
 /******************************************
