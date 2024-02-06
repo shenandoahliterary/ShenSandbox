be8117524f2664e54a9614c41625e31a2b5e459f
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2024-02-06 13:25:03 -0500

    Modified hardcoded values

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 		<div class="row nav-bar-row" style="margin-top:.25vw; padding-top:.5vw">
 			<div class="col-md-4">
 				<div class="volumeIssueBanner" style="padding-left: 6vw">
-					Volume 73, Number 1 &middot; Fall 2023
+					Volume xx, Number x &middot; Fall xxxx
 				</div>
 			</div>
 
